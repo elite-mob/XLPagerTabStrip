acb3797d471ca86b5ac3e0988669ac5448dd36bf
Elite-Mob <unisoftmw@gmail.com>
2023-05-29 23:34:17 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,16 +1,32 @@
-// swift-tools-version:5.1
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// // swift-tools-version:5.1
+// // The swift-tools-version declares the minimum version of Swift required to build this package.
 
+// import PackageDescription
+
+// let package = Package(
+//     name: "XLPagerTabStrip",
+//     products: [
+//         .library(name: "XLPagerTabStrip", targets: ["XLPagerTabStrip"]),
+//     ],
+//     dependencies: [
+//     ],
+//     targets: [
+//         .target(name: "XLPagerTabStrip", dependencies: [])
+//     ]
+// )
+
+// swift-tools-version:5.7
 import PackageDescription
 
 let package = Package(
-    name: "XLPagerTabStrip",
+    name: "PagerTabStripView",
     products: [
-        .library(name: "XLPagerTabStrip", targets: ["XLPagerTabStrip"]),
-    ],
-    dependencies: [
+        .library(name: "XLPagerTabStrip", targets: ["XLPagerTabStrip"])
     ],
     targets: [
-        .target(name: "XLPagerTabStrip", dependencies: [])
+        .target(
+            name: "XLPagerTabStrip",
+            path: "Sources"
+        ),
     ]
 )
